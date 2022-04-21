2071eeb4f53024bce182ef2f426ad7a17870b5a7
Tim Small <smalls.online@icloud.com>
2022-04-21 19:05:06 -0400

    Removed fonts

--- src/DeskHelper.Maui/MauiProgram.cs
+++ src/DeskHelper.Maui/MauiProgram.cs
@@ -7,15 +7,10 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.RegisterBlazorMauiWebView()
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
-
-		builder.Services.AddBlazorWebView();
+        builder
+            .RegisterBlazorMauiWebView()
+            .UseMauiApp<App>();
+        builder.Services.AddBlazorWebView();
 
 		return builder.Build();
 	}
