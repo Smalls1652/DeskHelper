cd4695061b1ba4b841dccbbeded94bf76c2e9718
Tim Small <smalls.online@icloud.com>
2022-04-21 16:28:07 -0400

    Changed how network adapters are filtered
    Depending on the platform the code is executing on, network adapters will filter differently.
    Primarily with macOS/*nix, since I have no way to (yet) determine if an adapter is physical or virtual.

--- src/DeskHelper.Blazor/components/NetworkAdapters/NetworkAdaptersList.razor.cs
+++ src/DeskHelper.Blazor/components/NetworkAdapters/NetworkAdaptersList.razor.cs
@@ -18,15 +18,25 @@ public partial class NetworkAdaptersList : ComponentBase
     {
         if (OnlyUpNetworkAdapters is true)
         {
+#if _WINDOWS
             _networkAdapters = InputNetworkAdapters.FindAll(
                 (NetworkAdapterInfo adapterInfo) => adapterInfo.InterfaceStatus is OperationalStatus.Up && adapterInfo.InterfaceIsPhysical is true
             );
+#else
+            _networkAdapters = InputNetworkAdapters.FindAll(
+                (NetworkAdapterInfo adapterInfo) => adapterInfo.InterfaceStatus is OperationalStatus.Up
+            );
+#endif
         }
         else
         {
+#if _WINDOWS
             _networkAdapters = InputNetworkAdapters.FindAll(
                 (NetworkAdapterInfo adapterInfo) => adapterInfo.InterfaceIsPhysical is true
             );
+#else
+            _networkAdapters = InputNetworkAdapters;
+#endif
         }
     }
 }
