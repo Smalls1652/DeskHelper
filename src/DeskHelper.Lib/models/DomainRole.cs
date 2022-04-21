d6580161fcd53f600ad348048b0be9380352f910
Timothy Small <smalls.online@icloud.com>
2022-04-21 13:20:47 -0400

    Added the remaning values for DomainRole

--- src/DeskHelper.Lib/models/DomainRole.cs
+++ src/DeskHelper.Lib/models/DomainRole.cs
@@ -3,5 +3,9 @@ namespace DeskHelper.Lib.Models;
 public enum DomainRole : ushort
 {
     StandaloneWorkstation = 0,
-    MemberWorkstation = 1
+    MemberWorkstation = 1,
+    StandaloneServer = 2,
+    MemberServer = 3,
+    BackupDomainController = 4,
+    PrimaryDomainController = 5
 }
