cb8342af705b279127c93d498e78ef1de7c3489e
Timothy Small <smalls.online@icloud.com>
2022-05-12 14:54:20 -0400

    Added AzureAdInfoCard component

---
+++ src/DeskHelper.Blazor/components/AzureAdInfo/AzureAdInfoCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using DeskHelper.Lib.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DeskHelper.Blazor.Components.AzureAdInfo;
+
+public partial class AzureAdInfoCard : ComponentBase
+{
+    [Parameter()]
+    [EditorRequired()]
+    public AadStatus InputAzureAdInfo { get; set; } = null!;
+}
