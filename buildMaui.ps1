1e0d095b2cb4498013f697b24e87968b4d7c6fb1
Timothy Small <smalls.online@icloud.com>
2022-05-12 16:40:31 -0400

    Added MAUI build script

---
+++ buildMaui.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param(
+)
+
+$_platform = $null
+switch ([System.OperatingSystem]::IsMacOS()) {
+    $true {
+        $_platform = "macOS"
+        break
+    }
+
+    Default {
+        $_platform = "Windows"
+        break
+    }
+}
+Write-Verbose "Target platform: $($_platform)"
+
+$slnFile = Join-Path -Path $PSScriptRoot -ChildPath "DeskHelper.Maui.Blazor.sln"
+$srcPath = Join-Path -Path $PSScriptRoot -ChildPath "src\DeskHelper.Maui.Blazor\"
+
+dotnet restore "$($slnFile)" --force --nologo
+dotnet clean "$($slnFile)" --configuration "Release" -noLogo
+
+switch ($_platform) {
+    "macOS" {
+        dotnet publish "$($srcPath)" --framework "net6.0-maccatalyst" --configuration "Release" --nologo
+        break
+    }
+
+    Default {
+        dotnet publish "$($srcPath)" --framework "net6.0-windows10.0.19041.0" --configuration "Release" --nologo
+        break
+    }
+}
