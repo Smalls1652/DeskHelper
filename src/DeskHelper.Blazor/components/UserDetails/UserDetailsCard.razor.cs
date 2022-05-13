20a0173a6aa51aa923405b2cad610990f9798d6c
Timothy Small <smalls.online@icloud.com>
2022-05-13 18:45:43 -0400

    Added copy to clipboard

--- src/DeskHelper.Blazor/components/UserDetails/UserDetailsCard.razor.cs
+++ src/DeskHelper.Blazor/components/UserDetails/UserDetailsCard.razor.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Microsoft.AspNetCore.Components;
 
 namespace DeskHelper.Blazor.Components.UserDetails;
@@ -7,4 +8,18 @@ public partial class UserDetailsCard : ComponentBase
     [Parameter()]
     [EditorRequired()]
     public string UserName { get; set; } = null!;
+
+    private string GetCopyText()
+    {
+        StringBuilder stringBuilder = new();
+        stringBuilder
+            .AppendLine("# 🧑‍💻 User details")
+            .AppendLine("")
+            .AppendLine("| Name | Value |")
+            .AppendLine("| --- | --- |")
+            .AppendLine($"| **Currently logged on user** | `{UserName}` |")
+            .AppendLine("");
+
+        return stringBuilder.ToString();
+    }
 }
