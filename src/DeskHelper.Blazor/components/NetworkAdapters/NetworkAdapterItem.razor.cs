db2e430fd88befb8707a33cac1c661bad81ef7e7
Timothy Small <smalls.online@icloud.com>
2022-05-13 18:22:04 -0400

    Added copy to clipboard for NetworkAdapterItem

--- src/DeskHelper.Blazor/components/NetworkAdapters/NetworkAdapterItem.razor.cs
+++ src/DeskHelper.Blazor/components/NetworkAdapters/NetworkAdapterItem.razor.cs
@@ -1,3 +1,5 @@
+using System.Net.NetworkInformation;
+using System.Text;
 using DeskHelper.Lib.Models;
 using Microsoft.AspNetCore.Components;
 using SmallsOnline.Subnetting.Lib.Models;
@@ -8,4 +10,36 @@ public partial class NetworkAdapterItem : ComponentBase
 {
     [Parameter()]
     public NetworkAdapterInfo AdapterItem { get; set; } = null!;
+
+    private string GetCopyText()
+    {
+        StringBuilder stringBuilder = new();
+        stringBuilder
+            .AppendLine($"# 🌐 Network adapter: {AdapterItem.InterfaceName}")
+            .AppendLine("")
+            .AppendLine("| Name | Value |")
+            .AppendLine("| --- | --- |")
+#if IsWindows
+            .AppendLine($"| **Is adapter up?** | {(AdapterItem.InterfaceStatus is OperationalStatus.Up ? "Yes" : "No")}");
+#else
+            .AppendLine($"| **Is adapter up? | {(AdapterItem.InterfaceHasIPv4Address ? "Yes" : "No")}");
+#endif
+
+#if IsWindows
+        if (AdapterItem.InterfaceStatus is OperationalStatus.Up)
+#else
+        if (AdapterItem.InterfaceHasIPv4Address is true)
+#endif
+        {
+            stringBuilder.AppendLine($"| **IP address** | `{AdapterItem.InterfaceIPv4Address}` |")
+                .AppendLine($"| **Gateway address** | `{AdapterItem.InterfaceIPv4Gateway}` |")
+                .AppendLine($"| **DNS servers** | `{(AdapterItem.InterfaceDNSServers!.Count is not 0 ? string.Join(", ", AdapterItem.InterfaceDNSServers!) : "Not set")}` |")
+                .AppendLine($"| **Network ID** | `{AdapterItem.InterfaceSubnetInfo}` |")
+                .AppendLine($"| **MAC address** | `{AdapterItem.InterfaceMACAddress}` |");
+        }
+
+        stringBuilder.AppendLine("");
+
+        return stringBuilder.ToString();
+    }
 }
