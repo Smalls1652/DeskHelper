0c40d90c121003958900d902badbf67370182800
Timothy Small <smalls.online@icloud.com>
2022-05-13 18:54:13 -0400

    Add copy to clipboard support (#2)
    * Added Markdig package
    
    Markdig is being used for converting Markdown text to HTML.
    
    * Added copy to clipboard button to all visible components
    
    Components with copy button: ComputerDetailsCard, UserDetailsCard, AzureAdInfoCard, and NetworkAdapterItem.

---
+++ src/DeskHelper.Blazor/components/core/CopyItemButton.razor.cs
@@ -0,0 +1,55 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DeskHelper.Blazor.Components.Core;
+
+public partial class CopyItemButton : ComponentBase
+{
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; } = null!;
+
+    [Parameter]
+    [EditorRequired]
+    public string TextToCopy { get; set; } = null!;
+
+    [Parameter]
+    public string ButtonPrimaryColorClass { get; set; } = "btn-dark";
+
+    [Parameter]
+    public string ButtonOnCopyColorClass { get; set; } = "btn-success";
+
+    private bool _copyInProgress = false;
+
+    private IJSObjectReference? _copyItemButtonJsModule;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _copyItemButtonJsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/DeskHelper.Blazor/components/core/CopyItemButton.razor.js");
+
+        await base.OnInitializedAsync();
+    }
+
+    private async void CopyTextToClipboard()
+    {
+        if (_copyInProgress is false)
+        {
+            string convertedText = Markdown.ToHtml(
+                markdown: TextToCopy,
+                pipeline: new MarkdownPipelineBuilder()
+                    .UsePipeTables()
+                    .Build()
+            );
+
+            await _copyItemButtonJsModule!.InvokeVoidAsync("copyTextToClipboard", convertedText);
+
+            _copyInProgress = true;
+            StateHasChanged();
+            await Task.Delay(2000);
+
+            _copyInProgress = false;
+            StateHasChanged();
+            await Task.Delay(100);
+        }
+    }
+}
